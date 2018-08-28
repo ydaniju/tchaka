f21a252966d6b9c18aaf0255a6d39c0a027949d5
Yusuf Daniju <danijuyusuf@outlook.com>
2018-08-28 22:14:41 +0100

    test ftest user creation return valueor successful creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def create
+    user = User.create!(user_params)
+
+    render json: user, status: :created
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :role)
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,10 +5,17 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   fixtures :users
 
-  test 'valid user creation' do
+  test 'valid user creation has status 201' do
     user = users(:valid)
-    post '/users', params: { name: user.name, email: user.email }
+    post '/users', params: { user: { name: user.name, email: user.email } }
 
     assert_equal 201, status
+  end
+
+  test 'valid user creation returns user object' do
+    user = users(:valid)
+    post '/users', params: { user: { name: user.name, email: user.email } }
+
+    assert_equal user.name, JSON.parse(body)['name']
   end
 end
