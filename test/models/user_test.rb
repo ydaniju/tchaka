573a2d825d185dd4b969a21daa4bf4a732904de6
Yusuf Daniju <danijuyusuf@outlook.com>
2018-08-28 21:15:13 +0100

    test valid and invalid users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,9 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'the truth' do
-    user = users(:default)
-
+  test 'valid user' do
+    user = users(:valid)
     assert user.valid?
+  end
+
+  test 'user with no email' do
+    user = users(:no_email)
+    assert_not user.valid?
   end
 end
